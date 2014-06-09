e73797b68c98d93a58e1047d78d0fb0ca84050ac
Chris Alley <chris@chrisalley.info>
2014-06-09 16:14:48 +1200

    Write spec for Enquiry model

--- spec/models/enquiry_spec.rb
+++ spec/models/enquiry_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Enquiry, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Enquiry, type: :model do
+  describe "fields" do
+    it { should respond_to(:type_of_work) }
+    it { should respond_to(:first_name) }
+    it { should respond_to(:last_name) }
+    it { should respond_to(:email_address) }
+    it { should respond_to(:telephone) }
+    it { should respond_to(:address_line_1) }
+    it { should respond_to(:address_line_2) }
+    it { should respond_to(:city) }
+    it { should respond_to(:state) }
+    it { should respond_to(:postal_code) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email_address) }
+  end
 end

9c5cf8f28f3d4dfe145361db3549866a6af4b13c
Chris Alley <chris@chrisalley.info>
2014-08-30 21:59:07 +1200

    Prevent reserved words from being used as page names

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,14 @@ class Page < ActiveRecord::Base
   include Sluggable
 
   validates :name, presence: true, length: { in: 1..75 }, uniqueness: true
+
+  before_validation :prevent_reserved_name
+
+  private
+
+  def prevent_reserved_name
+    if self.slug.in?(['gallery', 'log-in', 'log-out', 'pages', 'photos', 'users'])
+      self.errors.add(name, 'is a reserved word. Choose a different name.')
+    end
+  end
 end
